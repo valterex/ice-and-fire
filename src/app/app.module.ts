9c883489c8dea22399552947e1ec2fe4003bba5e
Valter <vcvijic@gmail.com>
2021-02-20 18:40:37 +0100

    add app routing and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BooksModule } from '@app/books/books.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoadingComponent } from './shared/loading/loading.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, LoadingComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BooksModule,
+    MatToolbarModule,
   ],
+  exports: [LoadingComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
